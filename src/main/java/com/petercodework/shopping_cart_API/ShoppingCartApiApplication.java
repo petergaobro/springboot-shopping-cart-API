06e3d9fd4261c13b01587e7458030bdda7250cba
Peter GAO <37321872+petergaobro@users.noreply.github.com>
2025-05-10 13:27:05 +1000

    initial commit generate project

---
+++ src/main/java/com/petercodework/shopping_cart_API/ShoppingCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.petercodework.shopping_cart_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petercodework/shopping_cart_API/ShoppingCartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petercodework.shopping_cart_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
